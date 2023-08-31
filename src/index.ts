c03ff12e1a50d8573ed15c4df86ceb2ea7c2f816
Michihiro Ota <mo@blueoath.jp>
2023-08-31 10:39:51 +0900

    feat: export admin

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import * as admin from 'firebase-admin';
+import * as adminCore from 'firebase-admin';
 
 export * from './firebase/firebase.module';
 export * from './firebase/firebase.service';
 export * from './firebase/firebase.middleware';
+export const admin = adminCore;
+
+admin.firestore
 
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Express {
         interface Request {
-            user: admin.auth.DecodedIdToken;
+            user: adminCore.auth.DecodedIdToken;
         }
     }
 }
